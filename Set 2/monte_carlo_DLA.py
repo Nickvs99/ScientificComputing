8046841cb497dac0475b6906d3fddeec294fcbf2
Nick van Santen <vansantennick@gmail.com>
2022-03-01 19:53:58 +0100

    Added sticking probability

--- Set 2/monte_carlo_DLA.py
+++ Set 2/monte_carlo_DLA.py
@@ -10,7 +10,7 @@ class MonteCarloDLA():
     random walkers.
     """
 
-    def __init__(self, N=100):
+    def __init__(self, N=100, sticking_probability=0.1):
 
         self.N = N
         self.grid = np.zeros((N, N))
@@ -23,25 +23,37 @@ class MonteCarloDLA():
         # The heighest index of the structure
         self.structure_top_index = 0
 
+        self.sticking_probability = sticking_probability
+
+    def run(self):
+        while self.running:
+            self.update()
+
     def update(self):
 
-        walker = RandomWalker(self.generate_random_walker_position(), bounds=[self.N, self.structure_top_index + 1])
+        # The walker is initialized at one row above the structure. Its y-bounds is one extra layer tall,
+        # otherwise it can get stuck when the structure is immediatly underneath it at initilization.
+        walker = RandomWalker(self.generate_random_walker_position(), bounds=[self.N, self.structure_top_index + 2])
 
         while True:
             
-            walker.update()
-
-            if not 0 < walker.position[1] < self.N - 1:
+            walker.update(self.grid)
                 
-                # Respawn walker
+            # Respawn walker when out of bounds
+            if not 0 < walker.position[1] < self.N - 1:
                 walker.position = self.generate_random_walker_position()
 
             if self.is_walker_connected(walker):
                 
+                if random.random() > self.sticking_probability:
+                    continue
+
                 # Add position of walker to the structure
                 self.grid[walker.position[1]][walker.position[0]] = 1
 
                 self.structure_top_index = max(self.structure_top_index, walker.position[1])
+                
+                # Simulation stops when structure has reached the top
                 if walker.position[1] == self.N - 1:
                     self.running = False
 
@@ -65,8 +77,7 @@ class MonteCarloDLA():
     def animate(self):
 
         fig = plt.figure()
-        self.im = plt.imshow(self.grid, cmap='bone', origin='lower', extent=(0, 1, 0, 1), animated=True)
-
+        self.im = plt.imshow(self.grid, cmap='bone', origin='lower', animated=True)
         self.ani = animation.FuncAnimation(fig, self.update_frame, interval=1, blit=True)
         plt.show()
 
@@ -80,22 +91,29 @@ class MonteCarloDLA():
         self.im.set_array(self.grid)
         return self.im,
 
+    def create_plot(self):
+        fig = plt.figure()
+        plt.imshow(self.grid, cmap='bone', origin='lower')
+        plt.title(f"p = {self.sticking_probability}")
+
 class RandomWalker():
 
     def __init__(self, position, bounds):
         self.position = position
         self.bounds = bounds
     
-    def update(self):
+    def update(self, grid):
+
+        neighbours = self.get_neighbours()
         
-        # Get a random move: left, right, top, or bottom
-        move = random.choice([[-1, 0], [1, 0], [0, 1], [0, -1]])
-
-        for i in range(2):
-            self.position[i] += move[i]
-
-        # periodic boundaries in the x-direction
-        self.position[0] = self.position[0] % self.bounds[0]
+        # Random walker can only move towards a cell which is not part of the structure
+        while True:
+            temp_position = random.choice(neighbours)
+            
+            if grid[temp_position[1]][temp_position[0]] == 0:
+                break
+            
+        self.position = [temp_position[0], temp_position[1]]
 
     def get_neighbours(self):
         
@@ -119,8 +137,15 @@ class RandomWalker():
 
 def main():
 
-    sim = MonteCarloDLA(N=200)
-    sim.animate()    
+    ps = np.arange(0.1, 1.1, 0.1)
+    for p in ps:
+        print(f"\rRunning sticking probability: {p:.2f}", end="")
+        
+        sim = MonteCarloDLA(N=100, sticking_probability=p)
+        sim.run()
+        sim.create_plot()   
+    
+    plt.show()
 
 if __name__ == "__main__":
     main()
